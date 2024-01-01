17bb573728a39fc057598d5b1e1875e9c2d12d9c
Slee Woo <mail@sleewoo.com>
2024-01-01 14:18:58 +0200

    build client templates on request

---
+++ lib/client/vite.config-templates.ts
@@ -0,0 +1,107 @@
+
+import { basename, join, resolve } from "path";
+import { readdir, writeFile } from "fs/promises";
+
+import { type UserConfig, defineConfig } from "vite";
+
+import { sfcDts } from "./sfc";
+
+type FindReplace = [
+  find: string | RegExp,
+  replace: string | RegExp | Function
+]
+
+const replaceMap: Record<
+  string,
+  FindReplace[]
+> = {
+
+  "Layout.vue": [
+    [
+      /^/,
+      `import type { ItemT } from "@crud:virtual-module-placeholder/base";\n`,
+    ],
+  ],
+
+  "CreateDialog.vue": [
+    [
+      /^/,
+      `import type { ItemT, ItemI } from "@crud:virtual-module-placeholder/base";\n`,
+    ],
+
+  ],
+
+}
+
+export default defineConfig(async (): Promise<UserConfig> => {
+
+  for (
+    const entry of await readdir(
+      resolve(__dirname, "templates"),
+      { withFileTypes: true }
+    )
+  ) {
+
+    if (!entry.isFile() || !/\.vue$/.test(entry.name)) {
+      continue
+    }
+
+    for (
+      let { name, text } of sfcDts(
+        join(entry.path, entry.name)
+      )
+    ) {
+
+      if (basename(name) !== entry.name + ".d.ts") {
+        continue
+      }
+
+      for (const [ find, replace ] of replaceMap[entry.name] || []) {
+        text = tryReplace(basename(name), text, find, replace)
+      }
+
+      await writeFile(
+        resolve(__dirname, `templates/${ basename(name) }`),
+        text,
+        "utf8"
+      )
+
+    }
+
+  }
+
+  return {
+
+    build: {
+      minify: false,
+      outDir: "../../pkg/client",
+      emptyOutDir: false,
+      lib: {
+        entry: resolve(__dirname, "templates.ts"),
+        formats: [ "es" ],
+        fileName: "templates",
+      }
+    },
+
+  }
+})
+
+
+function tryReplace(
+  file: string,
+  text: string,
+  find: FindReplace[0],
+  replace: FindReplace[1],
+) {
+  const before = text
+  const after = text.replace(
+    find,
+    // @ts-expect-error
+    replace,
+  )
+  if (after === before) {
+    throw `${ file } - failed replacing ${ JSON.stringify(find.toString()) }`
+  }
+  return after
+}
+
--- lib/client/vite.config.ts
+++ lib/client/vite.config.ts
@@ -1,111 +1,25 @@
 
-import { basename, join, resolve } from "path";
-import { readdir, writeFile } from "fs/promises";
+import { resolve } from "path";
 
-import { type UserConfig, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
-import { sfcDts } from "./sfc";
 import { peerDependencies } from "../../package.json"; 
 
-type FindReplace = [
-  find: string | RegExp,
-  replace: string | RegExp | Function
-]
+export default defineConfig({
 
-const replaceMap: Record<
-  string,
-  FindReplace[]
-> = {
-
-  "Layout.vue": [
-    [
-      /^/,
-      `import type { ItemT } from "@crud:virtual-module-placeholder/base";\n`,
-    ],
-  ],
-
-  "CreateDialog.vue": [
-    [
-      /^/,
-      `import type { ItemT, ItemI } from "@crud:virtual-module-placeholder/base";\n`,
-    ],
-
-  ],
-
-}
-
-export default defineConfig(async (): Promise<UserConfig> => {
-
-  for (
-    const entry of await readdir(
-      resolve(__dirname, "templates"),
-      { withFileTypes: true }
-    )
-  ) {
-
-    if (!entry.isFile() || !/\.vue$/.test(entry.name)) {
-      continue
-    }
-
-    for (
-      let { name, text } of sfcDts(
-        join(entry.path, entry.name)
-      )
-    ) {
-
-      if (basename(name) !== entry.name + ".d.ts") {
-        continue
-      }
-
-      for (const [ find, replace ] of replaceMap[entry.name] || []) {
-        text = tryReplace(basename(name), text, find, replace)
-      }
-
-      await writeFile(
-        resolve(__dirname, `templates/${ basename(name) }`),
-        text,
-        "utf8"
-      )
-
-    }
-
-  }
-
-  return {
-
-    build: {
-      minify: false,
-      outDir: "../../pkg/client",
-      emptyOutDir: true,
-      lib: {
-        entry: resolve(__dirname, "index.ts"),
-        formats: [ "es", "cjs" ],
-        fileName: (format, name) => `${ name }.${ format.replace("es", "mjs") }`,
-      },
-      rollupOptions: {
-        external: Object.keys(peerDependencies),
-      },
+  build: {
+    minify: false,
+    outDir: "../../pkg/client",
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, "index.ts"),
+      formats: [ "es", "cjs" ],
+      fileName: (format, name) => `${ name }.${ format.replace("es", "mjs") }`,
     },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+    },
+  },
 
-  }
 })
-
-
-function tryReplace(
-  file: string,
-  text: string,
-  find: FindReplace[0],
-  replace: FindReplace[1],
-) {
-  const before = text
-  const after = text.replace(
-    find,
-    // @ts-expect-error
-    replace,
-  )
-  if (after === before) {
-    throw `${ file } - failed replacing ${ JSON.stringify(find.toString()) }`
-  }
-  return after
-}
 
